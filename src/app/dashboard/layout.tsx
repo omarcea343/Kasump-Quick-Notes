e6fc882e116d0e90080bba891841101b24511f65
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-06-09 16:12:16 -0600

    Supa

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,43 @@
 import { DashboardNav } from "@/components/DashboardNav";
+import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+
+async function getData({
+  email,
+  id,
+  firstName,
+  lastName,
+  profileImage,
+}: {
+  email: string;
+  id: string;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  profileImage: string | undefined | null;
+}) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+      stripeCustomerId: true,
+    },
+  });
+
+  if (!user) {
+    const name = `${firstName ?? ""} ${lastName ?? ""}`;
+
+    await prisma.user.create({
+      data: {
+        id: id,
+        email: email,
+        name: name,
+      },
+    });
+  }
+}
 
 export default async function DashboardLayout({
   children,
@@ -13,6 +50,13 @@ export default async function DashboardLayout({
   if (!user) {
     return redirect("/");
   }
+  await getData({
+    email: user.email as string,
+    firstName: user.given_name as string,
+    id: user.id as string,
+    lastName: user.family_name as string,
+    profileImage: user.picture,
+  });
 
   return (
     <div className="flex flex-col space-y-6 mt-10">
