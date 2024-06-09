d1b53e7e7cea25f740acc5b73e7871e9c4323a17
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-06-08 21:52:51 -0600

    A

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kasump - Quick Notes",
   description: "Quick Notes for your daily needs",
-  icons: {
-    icon: "logo.svg",
-  },
 };
 
 export default function RootLayout({
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,14 @@ import { Button } from "./ui/button";
 import {
   RegisterLink,
   LoginLink,
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Image from "next/image";
 
-export function Navbar() {
+export async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
@@ -17,14 +22,20 @@ export function Navbar() {
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
 
-          <div className="flex items-center gap-x-5">
-            <LoginLink>
-              <Button>Sign In</Button>
-            </LoginLink>
-            <RegisterLink>
-              <Button variant="secondary">Sign Up</Button>
-            </RegisterLink>
-          </div>
+          {(await isAuthenticated()) ? (
+            <LogoutLink>
+              <Button>Log out</Button>
+            </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Sign In</Button>
+              </LoginLink>
+              <RegisterLink>
+                <Button variant="secondary">Sign Up</Button>
+              </RegisterLink>
+            </div>
+          )}
         </div>
       </div>
     </nav>
