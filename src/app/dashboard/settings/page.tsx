f803ad1f7f7e893f2c3ab65675c93d19aa950ff1
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-06-09 19:20:33 -0600

    A

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,114 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      name: true,
+      email: true,
+      colorScheme: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function SettingsPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user?.id as string);
+
+  return (
+    <div className="grid items-start gap-8">
+      <div className="flex items-center justify-between px-2">
+        <div className="grid gap-1">
+          <h1 className="text-3xl md:text-4xl">Settings</h1>
+          <p className="text-lg text-muted-foreground">Your profile settings</p>
+        </div>
+      </div>
+
+      <Card>
+        <form>
+          <CardHeader>
+            <CardTitle>General Data</CardTitle>
+            <CardDescription>
+              Please provide general information about yourself.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-2">
+              <div className="space-y-1">
+                <Label>Your Name</Label>
+                <Input
+                  name="name"
+                  type="text"
+                  id="name"
+                  placeholder="Your Name"
+                  defaultValue={data?.name ?? undefined}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label>Your Email</Label>
+                <Input
+                  name="email"
+                  type="text"
+                  id="email"
+                  placeholder="Your Email"
+                  disabled
+                  defaultValue={data?.email as string}
+                />
+              </div>
+
+              <div className="space-y-1">
+                <Label>Color Scheme</Label>
+                <Select name="color" defaultValue={data?.colorScheme}>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a color" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Color</SelectLabel>
+                      <SelectItem value="theme-green">Green</SelectItem>
+                      <SelectItem value="theme-blue">Blue</SelectItem>
+                      <SelectItem value="theme-violet">Violet</SelectItem>
+                      <SelectItem value="theme-rose">Rose</SelectItem>
+                      <SelectItem value="theme-orange">Orange</SelectItem>
+                      <SelectItem value="theme-red">Red</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <Button>Save now</Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
+}
