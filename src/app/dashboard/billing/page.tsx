e539b3a48083779aeed4dc5f75458e0ede72bd53
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-06-14 01:07:18 -0600

    A

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,12 +1,52 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { CheckCircle2 } from "lucide-react";
+
+const featureItems = [
+  { name: "Unlimited Notes" },
+  { name: "Unlimited Storage" },
+  { name: "Custom Themes" },
+  { name: "Dark Mode" },
+  { name: "Links" },
+  { name: "Tags" },
+  { name: "Categories" },
+];
+
 export default function BillingPage() {
   return (
-    <div className="grid items-start gap-8">
-      <div className="flex items-center justify-between px-2">
-        <div className="grid gap-1">
-          <h1 className="text-3xl md:text-4xl">Billing</h1>
-          <p className="text-lg text-muted-foreground">Your billing settings</p>
+    <div className="max-w-md mx-auto space-y-4">
+      <Card className="flex flex-col">
+        <CardContent className="py-8">
+          <div>
+            <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-primary/10 text-primary">
+              Monthly
+            </h3>
+          </div>
+
+          <div className="mt-4 flex items-baseline text-6xl font-extrabold">
+            $30 <span className="ml-1 text-2xl text-muted-foreground">/mo</span>
+          </div>
+          <p className="mt-5 text-lg text-muted-foreground">
+            Write as many notes as you want
+          </p>
+        </CardContent>
+        <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:pb-10 sm:pt-6">
+          <ul className="space-y-4">
+            {featureItems.map((item, index) => (
+              <li key={index} className="flex items-center">
+                <div className="flex-shrink-0">
+                  <CheckCircle2 className="h-6 w-6 text-green-500" />
+                </div>
+                <p className="ml-3 text-base">{item.name}</p>
+              </li>
+            ))}
+          </ul>
+
+          <form className="w-full">
+            <Button className="w-full">Get Now</Button>
+          </form>
         </div>
-      </div>
+      </Card>
     </div>
   );
 }
