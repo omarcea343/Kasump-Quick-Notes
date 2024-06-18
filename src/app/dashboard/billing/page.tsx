3137f242c24bb92e6417688a6d9da023a63a68a4
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-06-18 15:57:17 -0600

    A

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -65,6 +65,21 @@ export default async function BillingPage() {
     return redirect(subscriptionUrl);
   }
 
+  if (data?.status === "active") {
+    return (
+      <div className="grid items-start gap-8">
+        <div className="flex items-center justify-between px-2">
+          <div className="grid gap-1">
+            <h1 className="text-3xl md:text-4xl">Subscription</h1>
+            <p className="text-lg text-muted-foreground">
+              Settings regarding your subscription
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-md mx-auto space-y-4">
       <Card className="flex flex-col">
